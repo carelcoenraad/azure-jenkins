1d4eb89edadb61ba5c9b6996c8eb2b1e089dcb32
Carel Coenraad <carel.coenraad@capgemini.com>
2020-04-02 16:33:53 +0200

    feat: add debug information

--- event-hubs/send.js
+++ event-hubs/send.js
@@ -4,6 +4,14 @@ const { EventHubProducerClient } = require('@azure/event-hubs');
 const connectionString = process.env.AZURE_EVENT_HUBS_CONNECTION_STRING;
 const eventHubName = process.env.AZURE_EVENT_HUBS_EVENT_HUB_NAME;
 
+if (!connectionString) {
+  console.error('Connection string is empty');
+}
+
+if (!eventHubName) {
+  console.error('Event hub name is empty');
+}
+
 async function main() {
   // Create a producer client to send messages to the event hub.
   const producer = new EventHubProducerClient(connectionString, eventHubName);
@@ -25,4 +33,5 @@ async function main() {
 
 main().catch((err) => {
   console.log('Error occurred: ', err);
+  process.exit(1);
 });
